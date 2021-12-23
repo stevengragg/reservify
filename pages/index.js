2cf3758a5805a78f28731f689e34ca0e4f3b51e8
sgragg5619 <sgragg5619@gmail.com>
2021-12-24 03:01:55 +0800

    Create Banner

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
 
             <Header />
             {/* Banner */}
+            <Banner />
         </div>
     );
 }
